48126099ed4d30439c8ab68755645ac900b69223
clarelewis-edi <s2879721@ed.ac.uk>
2025-09-22 12:00:58 +0100

    create r file and read in file

---
+++ proj1.r
@@ -0,0 +1,6 @@
+# Clare Lewis (s2879721), Grace  (s), Luke  (s)
+# brief description of each person's contribution and the % they did
+
+setwd("/Users/clarelewis/Documents/github/ext_stat_prog_group_work") ## comment out of submitted
+a <- scan("shakespeare.txt",what="character",skip=83,nlines=196043-83,
+          fileEncoding="UTF-8")
