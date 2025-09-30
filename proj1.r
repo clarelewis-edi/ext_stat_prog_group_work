6e4e5930be247cf8bfdc0fafc34d5b4b9f840678
clarelewis-edi <s2879721@ed.ac.uk>
2025-09-30 19:20:34 +0100

    update formatting to be consistent throughout

--- proj1.r
+++ proj1.r
@@ -8,239 +8,176 @@ a <- scan("shakespeare.txt",what="character",skip=83,nlines=196043-83,fileEncodi
 
 #### Look at the Shakespeare text ####
 
-#### 4-a ####
+#### 4A ####
 # Remove stage directions (indicated by'[_' and '_]')
 
 open_direction <- grep('\\[', a)  
 direction <- length(open_direction)
 
 direction_length <- rep(0, direction) 
-for(i in 1:direction) {direction_length[i] <- grep('\\]', a[open_direction[i]:(open_direction[i]+100)])[1]}  
+for (i in 1:direction) {
+  direction_length[i] <- grep('\\]', a[open_direction[i]:(open_direction[i] + 100)])[1]
+}  
 
 unclosed_bracket <- which(is.na(direction_length))
 direction_length[1338] <- 15
 direction_length[2026] <- 0
 
 close_direction <- rep(0, direction)  
-for(i in 1:direction){close_direction[i] <- (open_direction[i] + direction_length[i] -1)}  
+for (i in 1:direction) {
+  close_direction[i] <- (open_direction[i] + direction_length[i] - 1)
+}  
 
 ##which(duplicate[close_direction]) 
 
 direction_words <- rep(0, sum(direction_length)) 
-for(i in 1:direction){direction_words[(sum(direction_length[0:(i-1)])+1):(sum(direction_length[0:i]))] <- (open_direction[i]:close_direction[i])}
+for (i in 1:direction) {
+  direction_words[(sum(direction_length[0:(i - 1)]) + 1):(sum(direction_length[0:i]))] <- (open_direction[i]:close_direction[i])
+}
 
 a <- a[-direction_words]
 
 a <- gsub('\\[_', '', a)
 
-#### 4(d) ####
+#### 4B ####
+# Look for and remove cases where the entire word is uppercase (excluding 'I', 'O', and 'A')
 
-split_punc = function(text_vec, punc_vec){
-  
-  # Correct the format of the punctuation vector to be used in the "grep" function
-  punc_vec = paste(punc_vec, collapse = "|")
-  
-  # Find the indices of all words in the text vector that contain punctuation marks given in the punctuation vector
-  i_punc = grep(punc_vec, text_vec)
-  
-  # Create a holding vector for the elements of the text vector seperated from their corresponding punctuation, such that the length of this vector is the length of text + number of punc cases
-  # Note that this assumes that all words dont have two cases of punctuation in them. Sound assumption
-  text_vec_extra = rep(0, length(i_punc)+length(text_vec))
-  
-  # Indices in the holding vector which can be used as a reference to put the seperated punctuation as new elements
-  i_extra_elements = i_punc+1:length(i_punc)-1
-  
-  # Take out the last character of words with puncuation using the "substr" function and put them in their corresponding element in the new text vector
-  # Note: This assumes that all punctuation marks appear at the end of words. Sound assumption for the text file we are working with
-  text_vec_extra[i_extra_elements+1] = substr(text_vec[i_punc], nchar(text_vec[i_punc]), nchar(text_vec[i_punc]))
-  
-  # Create a copy of the text vector with all punctuation removed
-  text_vec_no_punc = gsub(pattern = punc_vec, replacement = "", text_vec)
-  
-  # Puts the elements/words without punctuation in their corresponding position in the new word vector
-  text_vec_extra[-i_extra_elements-1] = text_vec_no_punc
-  
-  # Return the new text vector with punctuation seperated from all elements and are noew considered there own unique element in the text vector
-  return(text_vec_extra)
-}
-
-# 4B
-
-# look for all uppercase words (excluding 'I', 'O', and 'A')
-# there are some instances of these single letter words with punctuation, we want to include these
-
-# WE WANT TO DO PUNCTUATION REMOVAL BEFORE THIS STEP TO INCLUDE INSTANCES OF 'I,' AS AN EXAMPLE
-uppercase_indices <- which(a == toupper(a) & a != 'A' & a != 'I' & a != 'O')# include all of the options after we remove punctuation
+uppercase_indices <- which(a == toupper(a) & !(a %in% c('A', 'I', 'O')))
 a <- a[-uppercase_indices]
 
-# return to this after we remove the punctuation
-# uppers <- unique(uppers[grep('I',uppers,fixed=TRUE)])
+#### 4C ####
+# Find and remove all words that contain a hyphen or an underscore 
 
-
-#### 4(c) ####
-
-# View all words that contain a hyphen or an underscore in "a" 
 a_punc_hyph = grep("-", a, fixed = T )
-
 a_punc_under = grep("_", a, fixed = T )
 
 # Remove all cases of hyphens and underscores in each word in the text
 a <- gsub(pattern = "[_-]", replacement = "", a)
 
-#### NOTE: Consider splitting hyphenated words into two separate words. May yield better predictive power and more sensensical prompts
+#### NOTE: Consider splitting hyphenated words into two separate words. May yield better predictive power and prompts
 
+#### 4D ####
+# Create function that will split all punctuation into their own entries
 
+split_punc <- function(text_vec, punc_vec){
+  # Correct the format of the punctuation vector to be used in the "grep" function
+  punc_vec <- paste(punc_vec, collapse = "|")
+  
+  # Find the indices of all words in the text vector that contain punctuation marks given in the punctuation vector
+  i_punc <- grep(punc_vec, text_vec)
+  
+  # Create a holding vector for the elements of the text vector separated from their corresponding punctuation, such that the length of this vector is the length of text + number of punc cases
+  # Note that this assumes that all words don't have two cases of punctuation in them. Sound assumption
+  text_vec_extra <- rep(0, length(i_punc) + length(text_vec))
+  
+  # Indices in the holding vector which can be used as a reference to put the separated punctuation as new elements
+  i_extra_elements <- i_punc + 1:length(i_punc) - 1
+  
+  # Take out the last character of words with punctuation using the "substr" function and put them in their corresponding element in the new text vector
+  # Note: This assumes that all punctuation marks appear at the end of words. Sound assumption for the text file we are working with
+  text_vec_extra[i_extra_elements + 1] <- substr(text_vec[i_punc], nchar(text_vec[i_punc]), nchar(text_vec[i_punc]))
+  
+  # Create a copy of the text vector with all punctuation removed
+  text_vec_no_punc <- gsub(pattern = punc_vec, replacement = "", text_vec)
+  
+  # Puts the elements/words without punctuation in their corresponding position in the new word vector
+  text_vec_extra[-i_extra_elements - 1] <- text_vec_no_punc
+  
+  # Return the new text vector with punctuation separated from all elements and are now considered there own unique element in the text vector
+  return(text_vec_extra)
+}
 
-#### 4(e) ####
-punc_vec = c(",","\\.",";","!",":","\\?")
-a = split_punc(a, punc_vec)
+#### 4E ####
+# Implement the function to separate punctuation
 
-#### 4-f ####
-#Make all the text lower case
+punc_vec <- c(",", "\\.", ";", "!", ":", "\\?")
+a <- split_punc(a, punc_vec)
+
+#### 4F ####
+# Convert all the text lower case
 
 a <- tolower(a)
 
-
-#### 5 ####
-#a  
+#### 5A ####
 #Created a vector of unique words in the text
+
 words <- unique(a)
 
-#b
+#### 5B ####
 # Indicates which location in the words vector each word in the full text corresponds to
+
 index <- match(a, words)
 
-#c
-# Counts the occurences of each word
+#### 5C ####
+# Counts the occurrences of each word
+
 word_occurences <- tabulate(index)
 
-#d
+#### 5D ####
 # Creates a vector of the 1000 most common words by using a for loop and rank to locate and order these words
-b<- rep(0, 1000)
-for(i in 1:1000){b[i] <- words[which((rank(word_occurences, ties.method = "first")) == (length(word_occurences) + 1 - i))]}
 
-#### 6 ####
-# a
-mlag <- 4
+b <- rep(0, 1000)
+for (i in 1:1000) {
+  b[i] <- words[which((rank(word_occurences, ties.method = "first")) == (length(word_occurences) + 1 - i))]
+}
+
+#### 6A ####
+
+mlag <- 4 # this value can be changed by the user to change the output results of the model
 token <- match(a,b)
 
-# b
-#### test
-d <- c(1:20)
-R <- matrix(NA,length(d)-mlag,mlag+1)
-for (i in 1:(length(d)-mlag)) {
-  for (j in 1:(mlag+1)) {
-    R[i,j] <- d[i + j - 1]
-  }
-}
+#### 6B ####
 
-## real
-M <- matrix(NA,length(a)-mlag,mlag+1)
-for (i in 1:(length(a)-mlag)) {
-  for (j in 1:(mlag+1)) {
+M <- matrix(NA, length(a) - mlag, mlag + 1)
+for (i in 1:(length(a) - mlag)) {
+  for (j in 1:(mlag + 1)) {
     M[i,j] <- token[i + j - 1]
   }
 }
-# 
-#### 7 - Old Code ####
-# 
-# key <- c("never", "die", ",", "but")
-# 
-# key_tokens <- rep(0,length(key))
-# 
-# for (i in 1:length(key)) {
-#   key_tokens[i] <- grep(paste0("^", key[i], "$"),b)
-# }
-# full_sentence <- key_tokens
-# 
-# repeat {
-#   for (i in 1:mlag) {
-#     matching_row_index <- which(apply(M[, i:mlag, drop = FALSE], 1, function(row) all(row == key_token)))
-#     if (length(matching_row_index) > 0) break
-#   }
-#   next_word <- M[matching_row_index,mlag+1]
-#   key_tokens <- c(key_tokens[2:mlag],sample(na.omit(next_word),1))
-#   full_sentence <- append(full_sentence,key_tokens[mlag])
-#   
-#   if (key_tokens[mlag] == match(".",b))
-#     break
-# }
-# output <- cat(b[full_sentence], sep=" ")
-# output
-# 
-# 
-# 
-# #### this is the instructions and code provided (we didnt use this code yet)
-# 
-# next.word <- function(key,M,M1,w=rep(1,ncol(M)-1)) {
-#   
-# }
-# where key is the word sequence for which the next word is to be generated, M is as defined above, M1 is
-# the vector of word tokens for the whole text and w is the vector of mixture weights (which actually don’t
-#                                                                                      need to be normalized).
-# The function should return a token for the next word, generated according to the
-# model described above. It should be able to deal appropriately with any length of key: using reduced order
-# versions of the model for short keys, and using only data from the end of key if it is too long
-# 
-# The crucial part of the function is (repeatedly) finding the rows of M that match key (or its reduced versions).
-# Suppose the current key is to be matched to columns mc:mlag of M. Now compute
-# 
-# ii <- colSums(!(t(M[,mc:mlag,drop=FALSE])==key))
-# If ii[j]=0 and is finite (see ?is.finite) then row j of M contains a match
-# 
-# 
 
 
-
-#### Section 7#####
-
-mlag <- 4   # Setting mlag (temporarily)
-# 
-M <- matrix(NA,length(a)-mlag,mlag+1)
-for (i in 1:(length(a)-mlag)) {
-  for (j in 1:(mlag+1)) {
-    M[i,j] <- token[i + j - 1]
-  }
-}
+#### 7 #####
 
 M1 <- token
+#key <-c() I think we dont need this command but need to test
+w = rep(1, ncol(M) - 1) # can be changed by the user
 
-key <-c()
-
-next.word <- function(key,M,M1,w=rep(1,ncol(M)-1)) { ## Returns NA for key = c()??
-  if(length(key) < mlag){key <- append(key, rep("", (mlag-length(key))), 0)}  # Ensures the key vector is at least as long as mlag before beginning - inserts blanks at the start of the vector if this is not already the case
+next.word <- function(key ,M, M1, w) { 
+  if (length(key) < mlag) {
+    key <- append(key, rep("", (mlag - length(key))), 0)
+  }  # Ensures the key vector is at least as long as mlag before beginning - inserts blanks at the start of the vector if this is not already the case
+  
   weights = c()
   next_token = c()
   
-  for(i in 1:mlag){  #Starting with 1, as 1:mlag will check for matches of mlag entried. the for loop will reduce matching requirements by 1 (2:mlag) if no mlag length matches found, and will continue to reduce by 1 until a match is found
-    token_key <- match(key[(length(key)-mlag+i):length(key)],b)  # Defines a vector of length mlag of the tokens of the words in the key vector. If the key is longer than mlag it only takes the last mlag entries
-    ii <- colSums(!(t(M[,i:mlag,drop=FALSE]) == token_key))  ## Code provided by SW; this will be equal to zero only if rows match completely (!() means that matches will be marked FALSE, and vice versa. As sych when summing matches will be adding 0, so any columns without full matches will be non-zero (and non-finite if NAs))
+  for (i in 1:mlag) {
+    #Starting with 1, as 1:mlag will check for matches of mlag entried. the for loop will reduce matching requirements by 1 (2:mlag) if no mlag length matches found, and will continue to reduce by 1 until a match is found
+    token_key <- match(key[(length(key) - mlag + i):length(key)], b)  # Defines a vector of length mlag of the tokens of the words in the key vector. If the key is longer than mlag it only takes the last mlag entries
+    ii <- colSums(!(t(M[ ,i:mlag, drop=FALSE]) == token_key))  ## Code provided by SW; this will be equal to zero only if rows match completely (!() means that matches will be marked FALSE, and vice versa. As sych when summing matches will be adding 0, so any columns without full matches will be non-zero (and non-finite if NAs))
     
     full_matches <- which(is.finite(ii) & ii == 0) # Returns the indices of the rows for which there is no NAs (is.finite()) and there is an exact match (== 0) 
     
-    next_word_tokens <- na.omit(M[full_matches,mlag+1]) # Creates a vector of the tokens for all the words which are preceded by an exact match 
+    next_word_tokens <- na.omit(M[full_matches, mlag + 1]) # Creates a vector of the tokens for all the words which are preceded by an exact match 
     weights <- append(weights, rep(w[i],length(next_word_tokens)))
     next_token <- append(next_token, next_word_tokens)
   }
-  if(length(next_token)==0){
-    next_word <- b[sample(na.omit(M1),1)]
+  
+  if (length(next_token) == 0) {
+    next_word <- b[sample(na.omit(M1), 1)]
     # Checks if there are tokens in nwt; if this is the case we will sample from one of these as these are the best match
     # sampled_word <- next_word_tokens[na.omit(sample(length(next_word_tokens)),1)] # Taking a sample token from the vector. We do this by sampling the length of the vector and taking the corresponding index of the vector - this works even when the vector only has a single entry (whereas a direct sample would take a sample from 1:(token number) instead of the entry itself)
     # next_word <- b[sampled_word] # Finding the word in b which corresponds to the sample word
-    
     return(next_word) # Once the next word has been defined, the function prints this and stops
   }
-  else{
+  else {
     next_word <- b[sample(next_token, size = 1, prob = weights)]
     return(next_word)
   }
 }
 
-# next.word(key_token,M,M1,w = c(1000,0,0,1))
-
 #### 8 ####
 
-starter.word.token <- function(start_word){ ###Defining a starter word token function
+starter.word.token <- function(start_word) { ###Defining a starter word token function
   punc_vec <- append(punc_vec, c('?', '.'))
   punc_tokens <- na.omit(b[punc_vec])
   
@@ -249,6 +186,7 @@ starter.word.token <- function(start_word){ ###Defining a starter word token fun
     start_word <- b[start_token]
     return(start_token)
   }
+  
   start_word <- tolower(start_word)
   
   if (start_word %in% punc_vec){
@@ -261,38 +199,38 @@ starter.word.token <- function(start_word){ ###Defining a starter word token fun
     start_token <- sample(na.omit(M1[! M1 %in% punc_tokens]), 1)
   }
   
-  else {start_token <- match(start_word, b)}
+  else {
+    start_token <- match(start_word, b)
+  }
+  
   return(start_token)
 }
-# starter.word <- function(start_word){
-#   if(missing(start_word)){start_word <- b[starter.word.token()]
-#   return(start_word)}
-#   else{return(start_word)}
-# }
 
 #### 9 ####
 
-sentence <- c()
+#punc_tokens <- na.omit(b[punc_vec]) do we need this line?
 
-#punc_tokens <- na.omit(b[punc_vec])
-
-generate.sentence <- function(sentence_prompt){
+generate.sentence <- function(sentence_prompt) {
   
-  if(missing(sentence_prompt)){sentence_prompt <- b[starter.word.token()]
+  if (missing(sentence_prompt)) {
+    sentence_prompt <- b[starter.word.token()]
   }
   
   sentence_prompt <- unlist(strsplit(sentence_prompt, split = " "))
   
   repeat{
-    next_word <- next.word(sentence_prompt, M, M1, w=c(1000, 100, 10, 1))
-    if(next_word %in% punc_vec & sentence_prompt[length(sentence_prompt)] %in% punc_vec){
-    } else
-    {sentence_prompt <- append(sentence_prompt, next_word)}
+    next_word <- next.word(sentence_prompt, M, M1, w=c(1000, 100, 10, 1)) # do we want to assign values to w here or outside the function
+    if (next_word %in% punc_vec & sentence_prompt[length(sentence_prompt)] %in% punc_vec) { # we have an empty if statement here?
+    }
+    else {
+      sentence_prompt <- append(sentence_prompt, next_word)
+    }
     
-    if(sentence_prompt[length(sentence_prompt)] == '.'){
+    if (sentence_prompt[length(sentence_prompt)] == '.') {
       break
     }
   }
+  
   full_sentence <- paste(sentence_prompt, collapse = " ")
   return(full_sentence)
 }
